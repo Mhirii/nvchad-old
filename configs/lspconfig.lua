e5ea4aa8ddfcd7761fa9e492a16b6afe4c1fbdb8
Mhirii <ahmedmhiri218@gmail.com>
2024-01-19 03:40:45 +0100

    feat rust lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,8 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "cssls", "clangd", "rust_analyzer", "cmake", "volar", "tsserver" }
+local servers = { "cssls", "clangd", "cmake", "volar", "tsserver" }
+--"rust_analyzer",
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
