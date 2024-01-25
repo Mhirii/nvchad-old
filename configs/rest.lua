65d5f4b1413a4aecdb909962ca2c36c4067911ae
Mhirii <ahmedmhiri218@gmail.com>
2024-01-25 13:13:05 +0100

    rest

--- configs/rest.lua
+++ configs/rest.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("rest-nvim").setup {
       -- Open request results in a horizontal split
-      result_split_horizontal = true,
+      result_split_horizontal = false,
       -- Keep the http file buffer above|left when split horizontal|vertical
       result_split_in_place = false,
       -- stay in current windows (.http file) or change to results window (default)
