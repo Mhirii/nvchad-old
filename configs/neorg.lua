3ce10c5b953824bb5e294a95cacf6268163505fc
Mhirii <ahmedmhiri218@gmail.com>
2024-02-05 22:19:35 +0100

    fix load method

--- configs/neorg.lua
+++ configs/neorg.lua
@@ -2,16 +2,16 @@ return {
   {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
-    event = "VeryLazy",
-    -- ft = "norg",
+    -- event = "VeryLazy",
+    ft = "norg",
     -- tag = "*",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("neorg").setup {
         load = {
-          ["core.defaults"] = {},  -- Loads default behaviour
+          ["core.defaults"] = {}, -- Loads default behaviour
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = {      -- Manages Neorg workspaces
+          ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
               workspaces = {
                 notes = "~/notes",
