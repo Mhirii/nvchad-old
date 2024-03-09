7842f78699d4053c91b39b24e7e92a5bb78db4cd
Mhirii <ahmedmhiri218@gmail.com>
2024-03-09 01:59:25 +0100

    fix harpoon

--- configs/harpoon.lua
+++ configs/harpoon.lua
@@ -15,19 +15,19 @@ return {
       end
 
       require("telescope.pickers")
-          .new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table {
-              results = file_paths,
-            },
-            previewer = conf.file_previewer {},
-            sorter = conf.generic_sorter {},
-          })
-          :find()
+        .new({}, {
+          prompt_title = "Harpoon",
+          finder = require("telescope.finders").new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
     end
 
-    vim.keymap.set("n", "<C-e>", function()
-      toggle_telescope(harpoon:list())
-    end, { desc = "Open harpoon window" })
+    -- vim.keymap.set("n", "<C-H>", function()
+    --   toggle_telescope(harpoon:list())
+    -- end, { desc = "Open harpoon window" })
   end,
 }
