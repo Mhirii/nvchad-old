bd6d3a376fde073fb1b9d28e0eca06a54b1dd01c
Mhirii <ahmedmhiri218@gmail.com>
2024-03-23 02:47:46 +0100

    auto fold ts

---
+++ configs/autoFoldImports.lua
@@ -0,0 +1,6 @@
+return {
+	"github-naresh/auto-fold-imports.nvim",
+	ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	event = { "BufReadPre", "BufNewFile" },
+	config = true,
+}
