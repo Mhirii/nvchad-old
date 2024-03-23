be50686a22c1b894f8bc6e2c44810f4b31e5ad86
Mhirii <ahmedmhiri218@gmail.com>
2024-03-23 02:48:20 +0100

    noice

--- configs/noice.lua
+++ configs/noice.lua
@@ -69,10 +69,10 @@ return {
 					view = "mini",
 				},
 				hover = {
-					enabled = false,
+					enabled = true,
 				},
 				signature = {
-					enabled = false,
+					enabled = true,
 				},
 				message = {
 					-- Messages shown by lsp servers
@@ -214,6 +214,7 @@ return {
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
+				inc_rename = settings.ui.inc_rename,
 			},
 		})
 	end,
