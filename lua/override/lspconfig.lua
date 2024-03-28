5b8d203def1e836061c358344055c9922d75132a
Mhirii <ahmedmhiri218@gmail.com>
2024-03-28 21:17:08 +0100

    override lspconfig

--- lua/override/lspconfig.lua
+++ lua/override/lspconfig.lua
@@ -16,21 +16,22 @@ M.on_attach = function(client, bufnr)
   map("n", "K", vim.lsp.buf.hover, opts "Lsp hover information")
   map("n", "gi", vim.lsp.buf.implementation, opts "Lsp Go to implementation")
   -- map("n", "<leader>sh", vim.lsp.buf.signature_help, opts("Lsp Show signature help"))
-  map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Lsp Add workspace folder")
-  map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Lsp Remove workspace folder")
+  -- map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Lsp Add workspace folder")
+  -- map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Lsp Remove workspace folder")
 
-  map("n", "<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts "Lsp List workspace folders")
+  -- map("n", "<leader>wl", function()
+  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, opts "Lsp List workspace folders")
 
   map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Lsp Go to type definition")
 
-  map("n", "<leader>ra", function()
-    require "nvchad.lsp.renamer"()
-  end, opts "Lsp NvRenamer")
+  -- map("n", "<leader>ra", function()
+  --   require "nvchad.lsp.renamer"()
+  -- end, opts "Lsp NvRenamer")
 
   map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts "Lsp Code action")
   -- map("n", "gr", vim.lsp.buf.references, opts("Lsp Show references"))
+  map("n", "gr", "<cmd>Telescope lsp_references<CR>", { desc = "Telescope - LSP References" })
 
   -- setup signature popup
   if conf.signature and client.server_capabilities.signatureHelpProvider then
