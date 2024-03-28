1e90881225f9130a17d8411244c299f180afebea
Mhirii <ahmedmhiri218@gmail.com>
2024-03-28 10:12:27 +0100

    fix lsp lines

--- lua/configs/utility/lsplines.lua
+++ lua/configs/utility/lsplines.lua
@@ -1,10 +1,16 @@
 return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  keys = {
+    {
+      "<leader>Ti",
+      function()
+        require("lsp_lines").toggle()
+      end,
+      desc = "LspLines - Toggle",
+      mode = { "n" },
+    },
+  },
   config = function()
     require("lsp_lines").setup()
-
-    vim.keymap.set("n", "<leader>Ti", function()
-      require("lsp_lines").toggle()
-    end, { desc = "LspLines - Toggle" })
   end,
 }
